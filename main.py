d671228c3a06c6365c473595d144bbd2a50a1947
Mikko Pohja <mikko.pohja@futurice.com>
2017-11-24 09:39:41 +0200

    Cleanup

--- main.py
+++ main.py
@@ -8,70 +8,57 @@ import threading
 
 from skimage import io
 
-DOWNSAMPLE_RATIO = 2
+face_recognition = dlib.face_recognition_model_v1('./dlib_face_recognition_resnet_model_v1.dat')
+face_detector = dlib.get_frontal_face_detector()
+shape_predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
 
-faceRecognition = dlib.face_recognition_model_v1('./dlib_face_recognition_resnet_model_v1.dat')
-faceDetector = dlib.get_frontal_face_detector()
-shapePredictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
+enrolled_faces = {}
 
-enrolledFaces = {}
+# Semaphor for identifying thread
+identifying = False
 
 def face_to_vector(image, face):
   return (
     np
-    .array(faceRecognition.compute_face_descriptor(image, face))
+    .array(face_recognition.compute_face_descriptor(image, face))
     .astype(float)
   )
-
-def upscaleFaceRect(rect, factor):
-  return dlib.rectangle(rect.left() * factor,
-                            rect.top() * factor,
-                            rect.right() * factor,
-                            rect.bottom() * factor)
-
 
 def faces_from_image(image):
   UPSAMPLING_FACTOR = 0
   faces = [
-    (face.height() * face.width(), shapePredictor(image, face))
-    for face in faceDetector(image, UPSAMPLING_FACTOR)
+    (face.height() * face.width(), shape_predictor(image, face))
+    for face in face_detector(image, UPSAMPLING_FACTOR)
   ]
   return [face for _, face in sorted(faces, reverse=True)]
-
-def largest_face_from_image(image):
-  faces = faces_from_image(image)
-  return faces[0] if faces else None
 
 def image_from_file(path):
   return io.imread(path)
 
 def identify(image):
-#  faceDetect = time.time()
-  face = largest_face_from_image(image)
-#  print("face detection took " + str(time.time() - faceDetect))
-#  faceVec = time.time()
-  faceVector = face_to_vector(image, face)
-#  print("Face to vector took " + str(time.time() - faceVec))
-
-#  faceCompare = time.time()
+  # Get all faces
+  faces = faces_from_image(image)
+  # Pick largest face
+  face = faces[0] if faces else None
+  
+  # Calculate face descriptor
+  descriptor = face_recognition.compute_face_descriptor(image, face)
+  face_vector = np.array(descriptor).astype(float)
 
   # THIS is probably hazardous as ordering may not be always the same?
-  enrollIdentifiers = np.array(list(enrolledFaces.keys()))
-  enrollMatrix = np.array(list(enrolledFaces.values()))
-  distances = compute_distances(enrollMatrix, faceVector)
-  closestIndex = np.argmin(distances)
-#  print("Face compare took " + str(time.time() - faceCompare))
+  enroll_identifiers = np.array(list(enrolled_faces.keys()))
+  enroll_matrix = np.array(list(enrolled_faces.values()))
 
-  return enrollIdentifiers[closestIndex], distances[closestIndex]
+  # Calculate differences between the face and all enrolled faces
+  differences = np.subtract(np.array(enroll_matrix), face_vector)
+  distances = np.linalg.norm(differences, axis=1)
+  # and pick the closest one
+  closest_index = np.argmin(distances)
 
-def compute_distances(matrix, vector):
-    differences = np.subtract(np.array(matrix), vector)
-    return np.linalg.norm(differences, axis=1)
+  return enroll_identifiers[closest_index], distances[closest_index]
 
-# Semaphor for identifying thread
-identifying = False
 
-def handleFrame(origFrame, cb):
+def handle_frame(origFrame, cb):
   global identifying
   try:
     frame = cv2.resize(origFrame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
@@ -105,20 +92,20 @@ def webcam(cb):
         break
       identifying = True
 
-      thread = threading.Thread(target=handleFrame, args=(frame, cb))
+      thread = threading.Thread(target=handle_frame, args=(frame, cb))
       thread.daemon=True
       thread.start()
 
   # When everything is done, release the capture
   video_capture.release()
 
-def loadEnrolledFaces():
-  global enrolledFaces
+def load_enrolled_faces():
+  global enrolled_faces
   if (not os.path.isfile('faces.npy')):
     print('No enrolled faces found')
   else:
     print('Enrolled faces loaded from faces.npy')
-    enrolledFaces = np.load('faces.npy').item()
+    enrolled_faces = np.load('faces.npy').item()
 
 def logger(identifier, distance, duration):
   if (identifier == '-'):
@@ -128,5 +115,5 @@ def logger(identifier, distance, duration):
   else:
     print(identifier, distance, duration)
 
-loadEnrolledFaces()
+load_enrolled_faces()
 webcam(logger)
